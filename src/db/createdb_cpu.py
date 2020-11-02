14ac2555eb96f36763cbb1f57a46169ac2e88daa
alpdoganturkoglu <alpdoganturkoglu@gmail.com>
2020-11-02 15:42:57 +0300

    fixed file names and clean coded

--- src/db/createdb_cpu.py
+++ src/db/createdb_cpu.py
@@ -16,8 +16,8 @@ import get_price
 cpu_link = []
 intel_data = []
 amd_data = []
+cpus = []
 temp_list = []
-
 
 if not os.path.exists('./csv/CPU.csv'):
     import download_file as df
@@ -27,27 +27,22 @@ if not os.path.exists('./driver/geckodriver.exe'):
     import download_file as df
     df.download_gecko_driver()
 
-# -------------------------------------------------CPU-------------------------------------------------------------- #
-cpus = []
-with open('./json/cpu.json') as f:
+with open('./json/CPU.json') as f:
     json_file = json.load(f)
-
 
 def intel_chipset(model):
     if model == "Model":
         pass
     else:
-
         series = re.findall("[0-9]", model, 1)
         series = series[0]
-
         code = model[len(model)-2:len(model)]
-        if re.search("[-][8-9]", model) and (re.search("0K", code) or re.search("0F", code) or re.search("KS",code) or re.search("KF", code) or re.search("6K",code) or re.search("[0-9][0-9]", code)):
+
+        if re.search("[-][8-9]", model) and (re.search("0K", code) or re.search("0F", code) or re.search("KS", code) or re.search("KF", code) or re.search("6K", code) or re.search("[0-9][0-9]", code)):
             oc = "Z370,Z390"
             no_oc = "H310,B365,B360,H370,Q370"
             pin = "LGA 1151"
             return oc, no_oc, pin
-
         elif re.search("[-][7]", model) and (re.search("X", code) or re.search("XE", code)):
             oc = "X299"
             no_oc = "X299"
@@ -58,24 +53,21 @@ def intel_chipset(model):
             no_oc = "B250,Q250,H270,Q270"
             pin = "LGA 1151"
             return oc, no_oc, pin
-
-        elif re.search("-10", model, 1) and (re.search("0K", code) or re.search("0F", code) or re.search("KF", code) or re.search("[0-9][0-9]", code),re.search("0X", code)):
+        elif re.search("-10", model, 1) and (re.search("0K", code) or re.search("0F", code) or re.search("KF", code) or re.search("[0-9][0-9]", code), re.search("0X", code)):
             oc = "Z490"
             no_oc = "H410,B460,H470,Q470,W480"
             pin = "LGA 1200"
             return oc, no_oc, pin
         else:
-            oc="Unknown"
-            no_oc="Unkown"
-            pin="Unkown"
-            return oc,no_oc,pin
-
+            oc = "Unknown"
+            no_oc = "Unkown"
+            pin = "Unkown"
+            return oc, no_oc, pin
 
 def amd_chipset(model):
     if model == "Model":
         pass
     else:
-
         if re.search("TR", model, 1):
             if(re.search("3", model)):
                 pin = "TRX4"
@@ -89,8 +81,9 @@ def amd_chipset(model):
             temp_gen = re.split("[0-9]", model, 1)
         else:
             temp_gen = model
-        gen = str(temp_gen[len(temp_gen) - 1]).strip()
 
+        gen = str(temp_gen[len(temp_gen) - 1]).strip()
+        
         if gen[len(gen) - 1] != "U" or "H" or "M":
             if gen[0] == "1" or (gen[0] == "2" and gen[len(gen) - 1] == "G"):
                 oc = "X370,B350,X470,B450"
@@ -105,7 +98,7 @@ def amd_chipset(model):
                 if(pin == "TR4"):
                     oc = "X399"
                     no_oc = "X399"
-                return oc,no_oc,pin
+                return oc, no_oc, pin
             elif gen[0] == "3":
                 oc = "X470,B450,X570,B550"
                 no_oc = "A520"
@@ -114,16 +107,15 @@ def amd_chipset(model):
                     no_oc = "TRX40"
                 return oc, no_oc, pin
             else:
-                oc="Unknown"
-                no_oc="Unkown"
-                pin="Unkown"
-                return oc,no_oc,pin
-        else:            
-            oc="Unknown"
-            no_oc="Unkown"
-            pin="Unkown"
-            return oc,no_oc,pin
-
+                oc = "Unknown"
+                no_oc = "Unkown"
+                pin = "Unkown"
+                return oc, no_oc, pin
+        else:
+            oc = "Unknown"
+            no_oc = "Unkown"
+            pin = "Unkown"
+            return oc, no_oc, pin
 
 with open('./csv/CPU.csv', newline='') as cpu_first_data:
     reader = csv.DictReader(cpu_first_data)
@@ -134,15 +126,13 @@ with open('./csv/CPU.csv', newline='') as cpu_first_data:
         if i <= 310:
             if (re.search("Ryzen", cpu["Model"])):
                 check = amd_chipset(cpu["Model"])
-
-                if check :
+                if check:
                     oc, no_oc, pin = amd_chipset(cpu["Model"])
                     cpu["Chipset OC"] = oc
                     cpu["Chipset"] = no_oc
                     cpu["Socket"] = pin
                 else:
                     pass
-                    
             elif re.search("Core", cpu["Model"]):
                 check = intel_chipset(cpu["Model"])
                 if check:
@@ -150,21 +140,16 @@ with open('./csv/CPU.csv', newline='') as cpu_first_data:
                     cpu["Chipset OC"] = oc
                     cpu["Chipset"] = no_oc
                     cpu["Socket"] = pin
-                    
                 else:
                     pass
-                   
-                    
             else:
                 pass
         else:
             break
 
-
 def cpu_price():
     driver = webdriver.Firefox(executable_path="./driver/geckodriver.exe")
     c = CurrencyConverter()
-
     for row in cpu_csv_file:
         if row["URL"] in json_file:
             if row["Brand"] == "AMD":
@@ -182,26 +167,22 @@ def cpu_price():
 
 cpu_price()
 
-
 client = MongoClient('mongodb://localhost:27017/')
 db = client['PcBuilder']
 for cpu in cpu_csv_file:
     if cpu["URL"] in json_file and cpu["Price"]:
         post = {
-            
             "Brand": cpu["Brand"],
             "Model": cpu["Model"],
             "URL": cpu["URL"],
             "Rank": int(cpu["Rank"]),
-            "Chipset OC":cpu["Chipset OC"],
-            "Chipset":cpu["Chipset"],
-            "Socket":cpu["Socket"],
+            "Chipset OC": cpu["Chipset OC"],
+            "Chipset": cpu["Chipset"],
+            "Socket": cpu["Socket"],
             "Gameplay Benchmark": json_file[cpu["URL"]]["Gameplay Benchmark"],
             "Desktop Benchmark": json_file[cpu["URL"]]["Desktop Benchmark"],
             "Workstation Benchmark": json_file[cpu["URL"]]["Workstation Benchmark"],
             "Price": cpu["Price"]
-
         }
         posts = db.CPU
         post_id = posts.insert_one(post).inserted_id
-
--- src/db/createdb_ssd.py
+++ src/db/createdb_ssd.py
@@ -7,17 +7,13 @@ from currency_converter import CurrencyConverter
 from selenium import webdriver
 from pymongo import MongoClient
 
-
-
 import sys
 sys.path.insert(1, os.getcwd() + '/src/helpers/')
 import get_price
 
-
 if not os.path.exists('./csv/SSD.csv'):
     import download_file as df
     df.download_csv("SSD")
-
 if not os.path.exists("./driver/geckodriver.exe"):
     import download_file as df
     df.download_gecko_driver()
@@ -26,28 +22,25 @@ with open('./csv/SSD.csv', newline='') as ssd_first_data:
     reader = csv.DictReader(ssd_first_data)
     ssd_csv_file = list(reader)
 
-
 def ssd_price():
-   
     driver = webdriver.Firefox(executable_path="./driver/geckodriver.exe")
     c = CurrencyConverter()
 
-    for i,row in enumerate(ssd_csv_file, start=1):
+    for i, row in enumerate(ssd_csv_file, start=1):
         if i == 150:
             break
         product = row["Brand"]+" "+row["Model"]
-        product = re.sub("\s","%20",product)
+        product = re.sub("\s", "%20", product)
         base_url = "https://pricespy.co.uk/search?search="
         last_url = base_url + product
-        
-        price = get_price.get_price(driver,c,last_url)
+        price = get_price.get_price(driver, c, last_url)
         if price:
             row["Price"] = price
         else:
             row["Price"] = " "
     driver.quit()
-
 ssd_price()
+
 def ssd_model_parser(data_set):
     try:
         if re.search('M.2', data_set):  # Checking for 'M.2'
@@ -55,10 +48,11 @@ def ssd_model_parser(data_set):
         else:
             m2 = False
         storage_size = re.findall('[0-9][0-9][0-9][G][B]', data_set) or re.findall('[0-9][T][B]', data_set) \
-                       or re.findall('[0-9][0-9][G][B]', data_set)
+            or re.findall('[0-9][0-9][G][B]', data_set)
         storage = storage_size[0]  # to parse storage
 
-        if re.search('NVMe', data_set):  # to parse ssd brand.Nvme always comes after model type.
+        # to parse ssd brand.Nvme always comes after model type.
+        if re.search('NVMe', data_set):
             temp = re.split('NVMe', data_set)
         elif re.search('PCIe', data_set):
             temp = re.split('PCIe', data_set)
@@ -68,30 +62,23 @@ def ssd_model_parser(data_set):
             temp = re.split(r"\b" + storage_size[0] + r"\b", data_set)
         else:
             temp = data_set
-
         model = temp[0]
-        
-
         return model, storage, m2
     except:
         pass  # if it's empty or it doesn't fit the pattern
 
-
-
-    #ssd_price()
-for i,data in enumerate(ssd_csv_file, start=1) :
+for i, data in enumerate(ssd_csv_file, start=1):
     if i <= 150:
         try:
-            
             check = ssd_model_parser(data["Model"])
-            if check :
+            if check:
                 model, storage, m2 = ssd_model_parser(data["Model"])
                 data["Model"] = model.strip()
-                data["Storage"]= storage.strip()
+                data["Storage"] = storage.strip()
                 data["M2"] = m2
                 print(data)
             else:
-                pass        
+                pass
         except:
             print("There is a problem with data = " + data[3])
             pass
@@ -101,7 +88,7 @@ for i,data in enumerate(ssd_csv_file, start=1) :
 client = MongoClient('mongodb://localhost:27017/')
 db = client['PcBuilder']
 
-for i,ssd in enumerate(ssd_csv_file, start=1):
+for i, ssd in enumerate(ssd_csv_file, start=1):
     if i == 150:
         break
     else:
@@ -111,10 +98,10 @@ for i,ssd in enumerate(ssd_csv_file, start=1):
                 "Model": ssd["Model"],
                 "URL": ssd["URL"],
                 "Storage": ssd["Storage"],
-                "M2":ssd["M2"],
+                "M2": ssd["M2"],
                 "Rank": int(ssd["Rank"]),
                 "Price": ssd["Price"],
-                "Benchmark":ssd["Benchmark"]
+                "Benchmark": ssd["Benchmark"]
             }
             posts = db.SSD
             print(post)
