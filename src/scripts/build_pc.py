ee928434b2d84f33138fd40466df9a2fb5d1872f
StarBuckR <ridvantulemen@gmail.com>
2020-11-11 22:44:12 +0300

    building pc function improved

--- src/scripts/build_pc.py
+++ src/scripts/build_pc.py
@@ -24,7 +24,7 @@ client = MongoClient('mongodb://localhost:27017/')
 db = client.PcBuilder
 gpus = db.GPU
 
-def build_pc(price, percentages, build_type = BuildType.Gaming.name, gpu_brand = GpuBrand.Nvidia.name, cpu_brand = CpuBrand.Intel.name):
+def build_pc(price, percentages, build_type = BuildType.Gaming.name, gpu_brand = None, cpu_brand = None):
     if percentages == None:
         percentages = get_percentages(build_type)
         if percentages == None: return
@@ -39,8 +39,8 @@ def build_pc(price, percentages, build_type = BuildType.Gaming.name, gpu_brand =
         percentages.cpu += percentages.cpu*10/100
 
     leftover_price, cpu = pick_cpu(full_price(percentages.cpu, price), build_type, cpu_brand, leftover_price)
-    leftover_price, motherboard = pick_motherboard(full_price(percentages.motherboard, price), cpu["Socket"], cpu["Chipset"], leftover_price)
-    leftover_price, ram = pick_ram(full_price(percentages.ram, price), leftover_price)
+    leftover_price, motherboard = pick_motherboard(full_price(percentages.motherboard, price), cpu["Socket"], cpu["Chipset OC"], cpu["Chipset"], leftover_price)
+    leftover_price, ram = pick_ram(full_price(percentages.ram, price), motherboard, leftover_price)
     leftover_price, gpu = pick_gpu(leftover_price, build_type, gpu_brand, leftover_price)
 
     print(leftover_price)
@@ -51,32 +51,56 @@ def build_pc(price, percentages, build_type = BuildType.Gaming.name, gpu_brand =
     print(ssd)
     print(hdd)
 
-def pick_cpu(spendable_price, build_type, cpu_brand, price):
-    benchmark = get_benchmark_text(build_type)
-    cpu = db.CPU.find({"Price":{"$lt": spendable_price}, "Brand": cpu_brand}).sort(benchmark, -1)[0]
-    return price - cpu["Price"], cpu
-
 def pick_ssd(spendable_price, price):
-    ssd = db.SSD.find({"Price": {"$lt": spendable_price}}).sort("Benchmark", -1)[0]
+    m2 = False
+    if spendable_price >= 90:
+        m2 = True
+    ssd = db.SSD.find_one({"Price": {"$lt": spendable_price + spendable_price/10}, "M2": m2, "Storage": {"$lt": spendable_price * 20}}, sort=[("Benchmark", -1)])
     return price - ssd["Price"], ssd
 
-def pick_motherboard(spendable_price, socket, chipset, price):
-    motherboard = db.MOTHERBOARD.find({"Price":{"$lt": spendable_price}, "Socket": socket.replace(" ", ""), "Chipset": chipset.split(",")[0]})[0]
+def pick_hdd(spendable_price, price):
+    hdd = db.HDD.find_one({"Price": {"$lt": spendable_price + spendable_price/10, "$gt": spendable_price - spendable_price/10}, "Storage": {"$lt": spendable_price * 40}}, sort=[("Storage", -1)])
+    return price - hdd["Price"], hdd   
+
+def pick_cpu(spendable_price, build_type, cpu_brand, price):
+    cpu = []
+    benchmark = get_benchmark_text(build_type)
+    if cpu_brand == None:
+        cpu = db.CPU.find_one({"Price":{"$lt": spendable_price}}, sort=[(benchmark, -1)])
+    else:
+        cpu = db.CPU.find_one({"Price":{"$lt": spendable_price}, "Brand": cpu_brand}, sort=[(benchmark, -1)])
+    return price - cpu["Price"], cpu
+
+def pick_motherboard(spendable_price, socket, chipset_oc, chipset, price):
+    atx = "ATX" if spendable_price > 50 else None
+    motherboard = db.MOTHERBOARD.find_one({"Price":{"$lt": spendable_price + spendable_price/10}, "Atx": atx, "Socket": socket.replace(" ", ""), "Chipset": {"$in": chipset_oc.split(",")}}, sort=[("MHZ", -1)])
+    if not motherboard:
+        motherboard = db.MOTHERBOARD.find_one({"Price":{"$lt": spendable_price}, "Atx": atx, "Socket": socket.replace(" ", ""), "Chipset": {"$in": chipset.split(",")}}, sort=[("MHZ", -1)])
+    if not motherboard:
+        print("Couldn't find a motherboard!")
+        import sys
+        sys.exit() # this will be changed in the production
+
     return price - motherboard["Price"], motherboard
 
-def pick_ram(spendable_price, price):
-    ram = db.RAM.find({"Price":{"$lt": spendable_price}})[0]
+def pick_ram(spendable_price, motherboard, price):
+    rams = db.RAM.find({"Price":{"$lt": spendable_price}, "Total Memory": {"$lt": motherboard["Memory Max"]}, "RAM Count" : {"$lt": motherboard["Memory Slots"]}, "MHZ": {"$lt": motherboard["MHZ"]}})
+
+    ram = rams[0]
+    for r in rams:
+        if r["CL"] / r["MHZ"] * 2000 < ram["CL"] / ram["MHZ"] * 2000 or r["Total Memory"] > ram["Total Memory"]:
+            ram = r
+
     return price - ram["Price"], ram
 
 def pick_gpu(spendable_price, build_type, gpu_brand, price):
     benchmark = get_benchmark_text(build_type)
-    gpu = db.GPU.find({"Price":{"$lt": spendable_price}, "Brand": gpu_brand,}).sort(benchmark, -1)[0]
+    if gpu_brand == None:
+        gpu = db.GPU.find({"Price":{"$lt": spendable_price}}).sort(benchmark, -1)[0]
+    else:
+        gpu = db.GPU.find({"Price":{"$lt": spendable_price}, "Brand": gpu_brand}).sort(benchmark, -1)[0]
     return price - gpu["Price"], gpu
-
-def pick_hdd(spendable_price, price):
-    hdd = db.HDD.find({"Price": {"$lt": spendable_price}})[0]
-    return price - hdd["Price"], hdd    
-
+ 
 # default percentage: gpu = 28%, cpu = 22%, ram = 10%, motherboard = 15%, ssd = 10%, hdd = 5%, psu_and_case = 10%
 def get_percentages(build_type):
     switcher = { 
